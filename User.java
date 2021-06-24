a780859fe1c9dfd3b4eee66fc8ba7d11858a5eec
santosh <santosh.ece06@gmail.com>
2021-06-24 11:17:31 +0530

    init

---
+++ User.java
@@ -0,0 +1,22 @@
+
+class User {
+    int id;
+    String name;
+    String email;
+    String mobile;
+}
+
+class Base {
+    String id;
+    String name;
+    String style;
+}
+
+class img extends Base {
+    String src;
+    String alt;
+}
+
+class h1 extends Base {
+
+}
